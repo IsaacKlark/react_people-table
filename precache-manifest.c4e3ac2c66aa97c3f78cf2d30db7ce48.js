cd611bcb282af27236de4e1c538ca416f041f627
IsaacKlark <isaackeklarke@gmail.com>
2020-01-01 23:40:37 +0200

    Updates

---
+++ precache-manifest.c4e3ac2c66aa97c3f78cf2d30db7ce48.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a3db2e0b61801e0572785380b1b52d7",
+    "url": "/react_people-table/index.html"
+  },
+  {
+    "revision": "86781c782a6e6d930225",
+    "url": "/react_people-table/static/js/2.6bf1178d.chunk.js"
+  },
+  {
+    "revision": "1bb96ded90f863e272f3",
+    "url": "/react_people-table/static/js/main.336d8581.chunk.js"
+  },
+  {
+    "revision": "c6b2e506936d419d6d52",
+    "url": "/react_people-table/static/js/runtime~main.b2b73302.js"
+  }
+]);
